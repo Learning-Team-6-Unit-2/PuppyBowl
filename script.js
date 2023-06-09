70b2c41e5d789eba0f08cba5f807e58af160e464
R-Kershaw <rkershaw90@gmail.com>
2023-06-08 21:32:21 -0400

    added fetchAllPlayers() code

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const playerContainer = document.getElementById('all-players-container');
 const newPlayerFormContainer = document.getElementById('new-player-form');
 
 // Add your cohort name to the cohortName variable below, replacing the 'COHORT-NAME' placeholder
-const cohortName = 'YOUR COHORT NAME HERE';
+const cohortName = '2302-ACC-PT-WEB-PT-A';
 // Use the APIURL variable for fetch requests
 const APIURL = `https://fsa-puppy-bowl.herokuapp.com/api/${cohortName}/`;
 
@@ -12,7 +12,10 @@ const APIURL = `https://fsa-puppy-bowl.herokuapp.com/api/${cohortName}/`;
  */
 const fetchAllPlayers = async () => {
     try {
-
+        const response = await fetch(`${APIURL}players/`);
+        const players = response.json();
+        console.log(players);
+        return players;
     } catch (err) {
         console.error('Uh oh, trouble fetching players!', err);
     }
